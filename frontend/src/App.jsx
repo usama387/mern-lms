9efa888199bf9d480b1b1c3faaa16a66d8a89916
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-13 21:37:28 +0500

    React router dom installed and initial routes arranged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import LoginPage from "./pages/Login";
 import Navbar from "./components/Navbar";
+import { HeroSection } from "./pages/student/HeroSection";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
 
 const App = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <>
+              <HeroSection />
+              {/* Courses */}
+            </>
+          ),
+        },
+        { path: "/login", element: <LoginPage /> },
+      ],
+    },
+  ]);
+
   return (
     <main>
-      <Navbar />
-      <LoginPage />
+    <RouterProvider router={appRouter}/>
     </main>
   );
 };
