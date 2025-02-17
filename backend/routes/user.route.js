bc4b69e5ea51f0951d8dbfd1d1cf9d911b8692db
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-17 13:17:02 +0500

    Logout and getProfile route added

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  getUserProfileDetails,
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import isUserAuthenticated from "../middlewares/isUserAuthenticated.js";
 
 const userRouter = express.Router();
 
 // Api routes for authentication, registration, user profile, etc.
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/logout", logoutUser)
+userRouter.get("/getProfile", isUserAuthenticated, getUserProfileDetails);
 
 export default userRouter;
