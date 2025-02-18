5817c1c51bbe6f57be6ca72940b8c728ab94ab1b
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-18 21:29:44 +0500

    Added a new Custom component that renders this LoadingSpinner component when redux loads user on page reload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { appStore } from "./redux/store";
 import { Toaster } from "./components/ui/sonner";
+import { appStore } from "./redux/store";
+import { useGetUserProfileDetailsQuery } from "./features/api/authApi";
+import LoadingSpinner from "./components/Loader";
+
+const Custom = ({ children }) => {
+  const { isLoading } = useGetUserProfileDetailsQuery();
+  return <>{isLoading ? <LoadingSpinner /> : <>{children}</>}</>;
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={appStore}>
-      <App />
-      <Toaster />
+      <Custom>
+        <App />
+        <Toaster />
+      </Custom>
     </Provider>
   </StrictMode>
 );
