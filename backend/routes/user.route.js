f3572b05f2787b38cf32c542f2384aea12509d82
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-18 16:03:40 +0500

    Added two new api routes to update user and logout user

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,15 +4,18 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  updateUserProfile,
 } from "../controllers/user.controller.js";
 import isUserAuthenticated from "../middlewares/isUserAuthenticated.js";
+import upload from "../utils/multer.js";
 
 const userRouter = express.Router();
 
 // Api routes for authentication, registration, user profile, etc.
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/logout", logoutUser)
+userRouter.post("/logout", logoutUser)
 userRouter.get("/getProfile", isUserAuthenticated, getUserProfileDetails);
+userRouter.put("/profile/updateProfile", isUserAuthenticated, upload.single("profilePicture"), updateUserProfile);
 
 export default userRouter;
