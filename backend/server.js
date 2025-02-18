8a34687ca89d4f11e806ae34d966a4a19df08f2b
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-18 16:02:21 +0500

    Modified CORS object

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
