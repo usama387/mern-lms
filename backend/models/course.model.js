e92e37d09d59252cd6fe15bb128263f1154ba340
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-19 21:21:29 +0500

    Course database model added using mongoose

---
+++ backend/models/course.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      enum: ["beginner", "intermediate", "advanced"],
+    },
+    price: {
+      type: Number,
+    },
+    thumbnail: {
+      type: String,
+    },
+    enrolledStudents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lectures: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lecture",
+      },
+    ],
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Course =
+  mongoose.models.course || mongoose.model("course", courseSchema);
