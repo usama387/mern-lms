7bca9037f42a734a60fb6e40f3252a89256f0e54
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-24 18:47:48 +0500

    Added courseApi as reducer

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import { authApi } from "@/features/api/authApi";
+import { courseApi } from "@/features/api/courseApi";
 
 // Combine all reducers into a single root reducer and the passed in store.js.
 const rootReducer = combineReducers({
   [authApi.reducerPath]: authApi.reducer,
+  [courseApi.reducerPath]: courseApi.reducer,
   auth: authReducer,
 });
 
