d77739fd1bd6c9cc302604bcab3fadf32dfb0f46
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-24 18:48:33 +0500

    Added courseApi as middleware in store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 import { authApi } from "@/features/api/authApi";
+import { courseApi } from "@/features/api/courseApi";
 
 // Create the Redux store with the root reducer (contains combined reducers) and middleware.
 export const appStore = configureStore({
   reducer: rootReducer,
   middleware: (defaultMiddleware) =>
-    defaultMiddleware().concat(authApi.middleware),
+    defaultMiddleware().concat(authApi.middleware, courseApi.middleware),
 });
 
 // to hit api end points whenever page is refreshed
