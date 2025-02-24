939c3824da5a32b6ed4a2063296f378ac6f0ef38
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-24 18:43:26 +0500

    Added new api route to get all courses created by admin

--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { createCourse } from "../controllers/course.controller.js";
+import {
+  createCourse,
+  getAllInstructorCreatedCourses,
+} from "../controllers/course.controller.js";
 import isUserAuthenticated from "../middlewares/isUserAuthenticated.js";
 
 const courseRouter = express.Router();
 
 courseRouter.post("/create", isUserAuthenticated, createCourse);
+courseRouter.get(
+  "/getCourses",
+  isUserAuthenticated,
+  getAllInstructorCreatedCourses
+);
 
 export default courseRouter;
