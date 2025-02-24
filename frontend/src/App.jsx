76d5c88dfa4b69b3fa3e479cf3ad219b25a0a13f
usama387 <128306049+usama387@users.noreply.github.com>
2025-02-24 18:46:53 +0500

    Added admin routes only visible to it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import MainLayout from "./layout/MainLayout";
 import Courses from "./pages/student/Courses";
 import MyCourses from "./pages/student/MyCourses";
 import Profile from "./pages/student/Profile";
+import Sidebar from "./pages/admin/Sidebar";
+import Dashboard from "./pages/admin/Dashboard";
+import CourseTable from "./pages/admin/course/CourseTable";
+import AddCourse from "./pages/admin/course/AddCourse";
 
 const App = () => {
   const appRouter = createBrowserRouter([
@@ -25,6 +29,26 @@ const App = () => {
         { path: "/login", element: <LoginPage /> },
         { path: "/my-courses", element: <MyCourses /> },
         { path: "/my-profile", element: <Profile /> },
+
+        // admin routes
+        {
+          path: "admin",
+          element: <Sidebar />,
+          children: [
+            {
+              path: "dashboard",
+              element: <Dashboard />,
+            },
+            {
+              path: "course",
+              element: <CourseTable />,
+            },
+            {
+              path: "course/create",
+              element: <AddCourse />,
+            },
+          ],
+        },
       ],
     },
   ]);
