51452092b10fe9d95d558fedfcf3e82bf76024a2
usama387 <128306049+usama387@users.noreply.github.com>
2025-03-10 12:52:49 +0500

    Added new api routes to get details and update a course

--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -2,8 +2,11 @@ import express from "express";
 import {
   createCourse,
   getAllInstructorCreatedCourses,
+  getCourseById,
+  updateCourse,
 } from "../controllers/course.controller.js";
 import isUserAuthenticated from "../middlewares/isUserAuthenticated.js";
+import upload from "../utils/multer.js";
 
 const courseRouter = express.Router();
 
@@ -12,6 +15,13 @@ courseRouter.get(
   "/getCourses",
   isUserAuthenticated,
   getAllInstructorCreatedCourses
+);
+courseRouter.get("/:courseId", isUserAuthenticated, getCourseById);
+courseRouter.put(
+  "/:courseId",
+  isUserAuthenticated,
+  upload.single("thumbnail"),
+  updateCourse
 );
 
 export default courseRouter;
