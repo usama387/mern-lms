62080f30a8318cc77dda51ff9186f9a2ff911b6f
usama387 <128306049+usama387@users.noreply.github.com>
2025-03-10 12:50:09 +0500

    Added a new route for admin to create a new lecture for a particular course using courseId

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import Dashboard from "./pages/admin/Dashboard";
 import CourseTable from "./pages/admin/course/CourseTable";
 import AddCourse from "./pages/admin/course/AddCourse";
 import EditCourse from "./pages/admin/course/EditCourse";
+import CreateLecture from "./pages/admin/lecture/CreateLecture";
 
 const App = () => {
   const appRouter = createBrowserRouter([
@@ -51,6 +52,10 @@ const App = () => {
             {
               path: "course/:courseId",
               element: <EditCourse />,
+            },
+            {
+              path: "course/:courseId/lecture",
+              element: <CreateLecture />,
             },
           ],
         },
