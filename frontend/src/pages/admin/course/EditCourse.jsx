dc5fd1fe0c574313ccaa849f48921554468a8743
usama387 <128306049+usama387@users.noreply.github.com>
2025-05-04 13:33:07 +0500

    Added a p tag to acknowledge admin

--- frontend/src/pages/admin/course/EditCourse.jsx
+++ frontend/src/pages/admin/course/EditCourse.jsx
@@ -7,11 +7,19 @@ const EditCourse = () => {
   return (
     <div className="mt-20">
       <div className="flex items-center justify-between mb-5">
-        <h1 className="text-gray-600 dark:text-emerald-500 text-xl font-bold">
-          Course Detailed Information
-        </h1>
+        <div className="flex flex-col gap-2 font-medium">
+          <h1 className="text-gray-600 dark:text-emerald-500 text-xl font-bold">
+            Course Detailed Information
+          </h1>
+          <p className="text-sm font-semibold">Add lectures to publish course.</p>
+        </div>
         <Link to="lecture">
-          <Button variant="outline" className="font-semibold transition hover:scale-105 duration-300 hover:text-blue-500 ring-2 ring-blue-500">Course Lectures</Button>
+          <Button
+            variant="outline"
+            className="font-semibold transition hover:scale-105 duration-300 hover:text-blue-500 ring-2 ring-blue-500"
+          >
+            Course Lectures
+          </Button>
         </Link>
       </div>
       <CourseTab />
