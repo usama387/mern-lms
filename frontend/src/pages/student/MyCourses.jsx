a489b5709ec8802edbe959c62310c35fee394ce6
usama387 <128306049+usama387@users.noreply.github.com>
2025-05-05 10:33:09 +0500

    Passed course as prop to show dummy data

--- frontend/src/pages/student/MyCourses.jsx
+++ frontend/src/pages/student/MyCourses.jsx
@@ -18,7 +18,7 @@ const MyCourses = () => {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {[1, 2, 3].map((course, index) => (
-              <Course key={index} />
+              <Course key={index} course={course} />
             ))}
           </div>
         )}
